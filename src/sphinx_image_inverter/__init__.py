2a6806ee7c1ce070564bd321637d3135feeecf2e
douden <d.denouden-vanderhorst@tudelft.nl>
2025-01-16 16:00:15 +0100

    Update __init__.py

--- src/sphinx_image_inverter/__init__.py
+++ src/sphinx_image_inverter/__init__.py
@@ -13,7 +13,7 @@ def copy_stylesheet(app: Sphinx, exc: None) -> None:
     image_filter = os.path.join(os.path.dirname(__file__), 'static', 'image_dark_mode.css')
     with open(image_filter,'r') as css:
         image_filter_content = css.read()
-    image_filter_content = image_filter_content.replace("<saturation>","1.0")
+    image_filter_content = image_filter_content.replace("<saturation>",str(app.config.inverter_saturation))
     if app.builder.format == 'html' and not exc:
         staticdir = os.path.join(app.builder.outdir, '_static')
         outfile = os.path.join(staticdir,'image_dark_mode.css')
@@ -24,6 +24,7 @@ def copy_stylesheet(app: Sphinx, exc: None) -> None:
 
 def setup(app: Sphinx):
     app.add_css_file('image_dark_mode.css')
+    app.add_config_value('inverter_saturation',1.5,'env')
     app.connect('build-finished', copy_stylesheet)
     return {
         "version": __version__,
